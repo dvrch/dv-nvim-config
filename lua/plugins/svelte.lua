f87eeb3609fa2771e38418b314dc5e13284aa709
k! <dvrchipro+klwpgk@gmail.com>
2025-10-05 02:41:33 +0200

    Updates plugin configurations
    Updates lazy.nvim and LazyVim commit hashes.
    
    Renames the mason.nvim plugin to mason-org/mason.nvim.
    
    Adds a new plugin configuration file for Svelte development, including treesitter support, LSP configuration, and svelte.nvim integration.

--- lua/plugins/svelte.lua
+++ lua/plugins/svelte.lua
@@ -1,8 +1,45 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "svelte" })
-    end
-  end,
+  -- Configuration pour Svelte
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "html", "css", "javascript", "typescript", "svelte" },
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    opts = {
+      ensure_installed = { "svelte", "tsserver" },
+      setup = {
+        svelte = function()
+          require("lspconfig").svelte.setup({})
+        end,
+      },
+    },
+  },
+  {
+    "evanleck/nvim-svelte",
+    ft = "svelte",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      -- Vos options de configuration svelte.nvim ici
+      -- Par exemple:
+      -- lsp = {
+      --   enabled = true,
+      --   auto_install = true,
+      -- },
+    },
+  },
 }
