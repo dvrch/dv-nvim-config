bec734aa8e7f8ddf290337fdff455fc03d99be02
k! <dvrchipro+klwpgk@gmail.com>
2025-10-07 09:34:46 +0200

    new frame rouge

--- lua/plugins/mermaid-preview.lua
+++ lua/plugins/mermaid-preview.lua
@@ -9,6 +9,14 @@ return {
     },
   },
   {
+    "edluffy/hologram.nvim",
+    config = function()
+      require('hologram').setup{
+        auto_display = true
+      }
+    end
+  },
+  {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
@@ -18,13 +26,19 @@ return {
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
       vim.g.mkdp_auto_start = 0
-      vim.g.mkdp_browser = ""
+      vim.g.mkdp_open_to_the_world = 0
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = "8888"
+      vim.g.mkdp_echo_preview_url = true
       vim.g.mkdp_preview_options = {
         mermaid = { theme = "default" }
       }
+      vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/nvim/colors/markdown.css")
+      vim.g.mkdp_page_title = "${name}"
+      vim.g.mkdp_preview_options.disable_sync_scroll = 1
     end,
     keys = {
-      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Aperçu Markdown/Mermaid" },
+      { "<leader>mp", "<cmd>split | terminal mmdc -i % -o %.png && hologram display %.png<cr>", desc = "Aperçu Mermaid (Kitty)" },
     },
   }
 }
