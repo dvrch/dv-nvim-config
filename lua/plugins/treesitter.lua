1e9a897a3f13ff396ee0ccb330eb83f55ef5b23f
k! <dvrchipro+klwpgk@gmail.com>
2025-10-07 21:24:21 +0200

    simplifié et rendu la configuration de Treesitter plus explicite, en   activant auto_install = true pour forcer l'installation si un analyseur   manque.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,13 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "mermaid",
-        "lua",
-        "vim",
-        "vimdoc",
-        "python",
-      },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require'nvim-treesitter.configs'.setup({
+      ensure_installed = { "markdown", "markdown_inline", "vimdoc", "lua", "python" },
+      sync_install = false,
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+    })
+  end
 }
