43835274ad9db32d9f04abb875323c4ec0f0d833
k! <dvrchipro+klwpgk@gmail.com>
2025-10-07 21:07:57 +0200

    le fichier lua/plugins/mermaid.lua. Je vais lire son   contenu, puis le corriger en supprimant complètement le bloc de   configuration orphelin.

--- lua/plugins/mermaid.lua
+++ lua/plugins/mermaid.lua
@@ -7,27 +7,4 @@ return {
       end
     end,
   },
-  {
-
-    ft = "markdown",
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    config = function()
-      vim.g.mkdp_auto_start = 0
-      vim.g.mkdp_auto_close = 1
-      vim.g.mkdp_refresh_slow = 0
-      vim.g.mkdp_command_for_global = 0
-      vim.g.mkdp_open_to_the_world = 0
-      vim.g.mkdp_browser = ""
-      vim.g.mkdp_preview_options = {
-        mermaid = {
-          theme = "default",
-        }
-      }
-    end,
-    keys = {
-      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Preview Markdown (with Mermaid)" },
-    },
-  },
 }
