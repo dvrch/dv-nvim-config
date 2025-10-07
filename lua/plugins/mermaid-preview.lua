563a63334b4c3a44334d1b02e0902e4433f58677
k! <dvrchipro+klwpgk@gmail.com>
2025-10-07 09:15:24 +0200

    encore la preview dns browser

--- lua/plugins/mermaid-preview.lua
+++ lua/plugins/mermaid-preview.lua
@@ -11,15 +11,20 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
     build = function()
       vim.fn["mkdp#util#install"]()
     end,
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
       vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_browser = ""
+      vim.g.mkdp_preview_options = {
+        mermaid = { theme = "default" }
+      }
     end,
     keys = {
-      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Preview Mermaid diagram" },
+      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Aperçu Markdown/Mermaid" },
     },
-  },
+  }
 }
---
+++ lua/plugins/mermaid.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "markdown", "mermaid" })
+      end
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_command_for_global = 0
+      vim.g.mkdp_open_to_the_world = 0
+      vim.g.mkdp_browser = ""
+      vim.g.mkdp_preview_options = {
+        mermaid = {
+          theme = "default",
+        }
+      }
+    end,
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Preview Markdown (with Mermaid)" },
+    },
+  },
+}
