5fa22216a908bce62c6464647b414489215f79f0
k! <dvrchipro+klwpgk@gmail.com>
2025-10-07 20:46:54 +0200

    :    1. Le fichier lua/plugins/dependencies.lua dit à Mason de s'assurer que mmdc       est installé.    2. Il n'y a plus de scripts annexes qui créent de la confusion.

--- lua/plugins/dependencies.lua
+++ lua/plugins/dependencies.lua
@@ -25,7 +25,7 @@ return {
     "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "mermaid-cli" })
+      vim.list_extend(opts.ensure_installed, { "mmdc" })
     end,
   }
 }
