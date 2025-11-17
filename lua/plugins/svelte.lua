362073660a8461af2ff9d2808812e91a3228fa49
k! <dvrchipro+klwpgk@gmail.com>
2025-11-17 17:00:26 +0100

    Adds Svelte support and in-editor web browsing
    Integrates Svelte language server and Treesitter for enhanced Svelte development.
    Introduces `w3m.vim` to enable in-editor web browsing.
    Adds `cyberdream` to the list of installable colorschemes.
    Updates plugin lockfile to reflect latest versions.

--- lua/plugins/svelte.lua
+++ lua/plugins/svelte.lua
@@ -1,30 +1,33 @@
+-- ~/.config/nvim/lua/plugins/svelte.lua
 return {
-  -- Configuration pour Svelte
+  -- Ensure nvim-treesitter is configured for svelte
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = { "html", "css", "javascript", "typescript", "svelte" },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-    },
+    build = ":TSUpdate", -- Ensure parsers are updated
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "svelte" })
+      end
+    end,
   },
+
+  -- Configure LSP for Svelte via nvim-lspconfig and mason-lspconfig
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
     opts = {
-      ensure_installed = { "svelte", "tsserver" },
-      setup = {
-        svelte = function()
-          require("lspconfig").svelte.setup({})
-        end,
+      servers = {
+        -- Ensure svelte-language-server is set up
+        svelte = {},
       },
     },
+    config = function(_, opts)
+      require("mason").setup(opts.mason or {})
+      require("mason-lspconfig").setup(opts.mason_lspconfig or {})
+      require("lspconfig").svelte.setup(opts.servers.svelte)
+    end,
   },
-
 }
---
+++ lua/plugins/web-browser.lua
@@ -0,0 +1,8 @@
+return {
+  "yuratomo/w3m.vim",
+  cmd = "W3m",
+  config = function()
+    -- Vous pouvez définir ici votre navigateur par défaut si nécessaire
+    vim.g.w3m_browser = 'w3m'
+  end,
+}
