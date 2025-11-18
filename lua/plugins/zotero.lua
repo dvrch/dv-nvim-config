365ed5f266e429fb0c140e2bd3b7064a8cd91e2c
k! <dvrchipro+klwpgk@gmail.com>
2025-11-18 15:32:02 +0100

    1. Plugin `vscode-opener` : J'ai supprimé le nom "vscode-opener" de la configuration. lazy.nvim       était confus et le cherchait sur internet. Maintenant, il le reconnaîtra comme un plugin local.    2. Plugin `zotcite` : J'ai trouvé la ligne enabled = false dans sa configuration et l'ai remplacée       par enabled = true

--- lua/plugins/zotero.lua
+++ lua/plugins/zotero.lua
@@ -1,7 +1,7 @@
 return {
   {
     "jalvesaq/zotcite",
-    enabled = false,
+    enabled = true,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
       vim.g.zotero_sql_path = "/home/dv/snap/zotero-snap/common/Zotero/zotero.sqlite"
