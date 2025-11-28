8aca89c721c3c17d99688203740e40a2d87c453f
k! <dvrchipro+klwpgk@gmail.com>
2025-11-28 23:14:22 +0100

    feat: Set working directory correctly on Neovim startup

--- lua/plugins/cwd.lua
+++ lua/plugins/cwd.lua
@@ -1,10 +1,14 @@
 return {
   {
-    event = "VimEnter",
+    -- Priorité élevée pour s'assurer qu'il s'exécute avant les plugins de détection de racine
+    priority = 1000,
+    -- Charger ce plugin immédiatement au démarrage, ne pas le charger paresseusement
+    lazy = false,
     config = function()
-      -- Vérifie si Neovim a été ouvert avec un répertoire comme argument
-      if vim.fn.argc() > 0 and vim.fn.isdirectory(vim.fn.argv()[1]) == 1 then
-        -- Change le répertoire de travail pour celui spécifié
+      -- Si Neovim a été ouvert avec un argument qui est un répertoire
+      local arg = vim.fn.argv()[1]
+      if vim.fn.argc() > 0 and arg and vim.fn.isdirectory(arg) == 1 then
+        -- Alors, on change le répertoire de travail pour cet argument
         vim.cmd("silent! cd " .. vim.fn.argv()[1])
       end
     end,
