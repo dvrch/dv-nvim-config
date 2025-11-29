d986d71bbb5e0ac818ad65cf92cb70b56c9ae4c2
k! <dvrchipro+klwpgk@gmail.com>
2025-11-29 18:52:51 +0100

    toujours incomplet

--- lua/plugins/pieces.lua
+++ lua/plugins/pieces.lua
@@ -2,6 +2,10 @@ return {
   "pieces-app/plugin_neovim",
   lazy = false,  -- Chargement immédiat
   priority = 1000, -- Haute priorité
+  ft = { 'python', 'lua', 'vim', 'markdown' }, -- Add filetype detection
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- Add plenary.nvim as a dependency
+  },
   config = function()
     require("pieces").setup({
       -- Configuration optionnelle
