b7f128fc3b8b75756b9133e7f2b5c39eb8d95a8b
k! <dvrchipro+klwpgk@gmail.com>
2025-11-30 11:14:29 +0100

    cwd

--- lua/plugins/cwd.lua
+++ lua/plugins/cwd.lua
@@ -1,18 +1,18 @@
 return {
-  name = "cwd-config", -- Add this line
-  -- Exécuter avec une haute priorité au démarrage
-  priority = 1000,
-  event = "VeryLazy", -- Add this line to make it a valid local plugin spec
-  config = function()
-    -- Si Neovim a été ouvert avec un répertoire comme argument
-    local arg = vim.fn.argv()[1]
-    if vim.fn.argc() > 0 and arg and vim.fn.isdirectory(arg) == 1 then
-      -- Attendre 10 millisecondes que tous les autres plugins aient fini leur initialisation
-      vim.defer_fn(function()
-        -- Forcer le changement de répertoire pour qu'il soit la commande finale
-        vim.cmd("silent! cd " .. arg)
-      end, 10)
-    end
-  end,
+  {
+    -- Exécuter avec une haute priorité au démarrage
+    
+    event = "VeryLazy", -- Add this line to make it a valid local plugin spec
+    config = function()
+      -- Si Neovim a été ouvert avec un répertoire comme argument
+      local arg = vim.fn.argv()[1]
+      if vim.fn.argc() > 0 and arg and vim.fn.isdirectory(arg) == 1 then
+        -- Attendre 10 millisecondes que tous les autres plugins aient fini leur initialisation
+        vim.defer_fn(function()
+          -- Forcer le changement de répertoire pour qu'il soit la commande finale
+          vim.cmd("silent! cd " .. arg)
+        end, 10)
+      end
+    end,
+  },
 }
-
