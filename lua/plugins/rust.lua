c7546647997e1bf21372760a3d50b4b1639672f6
k! <dvrchipro+klwpgk@gmail.com>
2026-02-08 15:54:24 +0100

    rst-New

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,75 +1,82 @@
 return {
+  -- 🦀 rustaceanvim: The modern Rust toolset (Replacing deprecated rust-tools)
   {
-    "simrat39/rust-tools.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    ft = { "rust" },
+    opts = {
+      server = {
+        on_attach = function(client, bufnr)
+          -- Custom keys can be added here
+          -- Default rustaceanvim keys: K for hover, gd for definition, etc.
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for On-the-Fly diagnostics
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      },
     },
-    opts = function(_, opts)
-      local rt = require("rust-tools")
-      opts.tools = opts.tools or {}
-
-      opts.server = opts.server or {}
-      require("lazyvim.util").lsp.on_attach(function(client, buffer)
-        if client.name == "rust_analyzer" then
-          -- Hover actions
-          vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = buffer })
-          -- Code action groups
-          vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = buffer })
-        end
-      end)
-    end,
     config = function(_, opts)
-      require("rust-tools").setup(opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("force", {}, opts or {})
     end,
   },
 
-  -- Debugger (DAP) configuration for CodeLLDB
+  -- 🐛 Debugger (DA) Configuration
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      "mason-org/mason.nvim", -- Fixed organization name
+      "mason-org/mason.nvim",
       "jay-babu/mason-nvim-dap.nvim",
     },
-    config = function() -- Changed from opts to config to avoid calling dap.setup()
+    config = function()
       local dap = require("dap")
       
-      -- Ensure codelldb is installed via Mason
+      -- Ensure codelldb debugger is installed via Mason
       require("mason-nvim-dap").setup({
         ensure_installed = { "codelldb" },
+        automatic_installation = true,
       })
 
-      -- Configure codelldb adapter
+      -- Configure adapter for codelldb
       dap.adapters.codelldb = {
-        type = "server",
-        host = "127.0.0.1",
+        type = 'server',
         port = "${port}",
         executable = {
-          command = "codelldb",
-          args = { "--port", "${port}" },
-        },
+          command = 'codelldb', 
+          args = {"--port", "${port}"},
+        }
       }
 
-      -- Configure Rust launch configurations for DAP
+      -- Rust launch configurations
       dap.configurations.rust = {
         {
           name = "Launch file",
           type = "codelldb",
           request = "launch",
           program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
           end,
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-        },
-        {
-          name = "Launch test",
-          type = "codelldb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to test executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
-          end,
-          args = { "--test" },
-          cwd = "${workspaceFolder}",
+          cwd = '${workspaceFolder}',
           stopOnEntry = false,
         },
       }
