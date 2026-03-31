6056117c5203013d9ca6c41b3d6b59d0282bae75
k! <dvrchipro+klwpgk@gmail.com>
2026-03-31 10:30:49 +0200

    pour vue img md

--- lua/plugins/image-viewer.lua
+++ lua/plugins/image-viewer.lua
@@ -1,3 +1,35 @@
 return {
-  "edluffy/hologram.nvim",
+  {
+    "3rd/image.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      backend = "kitty",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown", "vimwiki" },
+        },
+        neorg = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "norg" },
+        },
+      },
+      max_width = 100,
+      max_height = 20,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 40,
+      window_overlap_clear_enabled = false, -- recommencé pour Kitty
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      editor_only_render_when_focused = false,
+      tmux_show_boundary = false,
+    },
+  },
 }
