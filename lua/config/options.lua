5f8a4a1c99e1fc25e96b4140a1dd51cc4dd13322
k! <dvrchipro+klwpgk@gmail.com>
2026-04-02 15:36:59 +0200

    MàJ reeltime  1

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,13 +9,29 @@ vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.undofile = true -- Garde l'undo history (c'est utile)
 
--- Protection supplémentaire pour les notebooks
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = "*.ipynb",
-  callback = function()
-    vim.opt_local.swapfile = false
-    vim.opt_local.backup = false
-    vim.opt_local.writebackup = false
-    vim.b.swapfile_disabled = true -- Flag custom
-  end,
+
+-- 🚀 AUTO-SAVE & LIVE SYNC GLOBAL
+vim.opt.autowriteall = true   -- Sauvegarde tout automatiquement
+vim.opt.autoread = true       -- Recharge les fichiers modifiés à l'extérieur (Houdini/Git)
+vim.opt.updatetime = 300      -- Vitesse de réaction (300ms) pour CursorHold
+
+-- On sauvegarde dès qu'on sort d'un buffer ou qu'on quitte le focus
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "BufWinLeave" }, {
+    pattern = "*",
+    callback = function()
+        if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" then
+            vim.cmd("silent! wall")
+        end
+    end
 })
+
+-- On recharge tous les buffers quand on revient sur Neovim
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    pattern = "*",
+    callback = function()
+        if vim.fn.getcmdwintype() == "" then
+            vim.cmd("checktime")
+        end
+    end
+})
+
