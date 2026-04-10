bb1e8e03c5b22a621655fff9a2f7e3b3f35d4a02
k! <dvrchipro+klwpgk@gmail.com>
2026-04-10 10:30:06 +0200

    database init

---
+++ lua/plugins/database.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    dependencies = {
+      "kristijanhusak/vim-dadbod-ui",
+      "kristijanhusak/vim-dadbod-completion",
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+      "DB",
+    },
+    init = function()
+      -- Configuration de l'interface
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_show_database_icon = 1
+      vim.g.db_ui_force_echo_notifications = 1
+      vim.g.db_ui_win_position = "left"
+      vim.g.db_ui_winwidth = 40
+      
+      -- Emplacement des sauvegardes de requêtes pour l'historique
+      vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("os").getenv("HOME") .. "/.local/share/db_ui"
+    end,
+    config = function()
+      -- Auto-complétion SQL via vim-dadbod-completion
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function()
+          require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+        end,
+      })
+    end,
+  },
+}
